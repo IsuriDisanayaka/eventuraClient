156e1f2370873ad12a4feb6cff2abbdeeb639d4a
Isuri Disanayaka <isuriUmeshika1@gmail.com>
2024-05-11 20:06:24 +0530

    google authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import SchedulerCalendar from "./screen/Calendar";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
+  const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
   return (
-    <div className="App">
-      <ToastContainer />
-      <SchedulerCalendar />
-    </div>
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <div className="App">
+        <ToastContainer />
+        <SchedulerCalendar />
+      </div>
+    </GoogleOAuthProvider>
   );
 }
 
--- client/src/components/nav/Navbar.js
+++ client/src/components/nav/Navbar.js
@@ -1,16 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import logo from "../../assets/img/logo.png";
 import "./Navbar.css";
+import { useGoogleLogin } from "@react-oauth/google";
 
 const Navbar = () => {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("userData"))
+  );
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const googleLogin = useGoogleLogin({
+    onSuccess: (tokenResponse) => {
+      fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: {
+          Authorization: `Bearer ${tokenResponse.access_token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setUser(data);
+          localStorage.setItem("userData", JSON.stringify(data));
+          fetch("http://localhost:5000/google-login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              googleId: data.sub,
+              email: data.email,
+              firstName: data.given_name,
+              lastName: data.family_name,
+              picture: data.picture,
+            }),
+          })
+            .then((response) => response.json())
+            .then((data) => console.log("User saved:", data))
+            .catch((error) => console.error("Error saving user:", error));
+        });
+    },
+    onError: () => {
+      console.log("Login Failed");
+      setUser(null);
+      localStorage.removeItem("userData");
+    },
+    scope: "openid email profile",
+  });
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("userData");
+    setShowDropdown(false);
+  };
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <div className="navbar">
       <div className="navbar-logo">
-        <img src={logo} style={{ height: " 15vh" }} alt="Eventura Logo" />
+        <img src={logo} style={{ height: "15vh" }} alt="Eventura Logo" />
       </div>
       <div className="navbar-icon">
-        <FaUserCircle size={30} />
+        {user ? (
+          <div>
+            <img
+              src={user.picture || "/path/to/default/avatar"}
+              alt="User"
+              style={{
+                width: 30,
+                height: 30,
+                borderRadius: "50%",
+                cursor: "pointer",
+              }}
+              onClick={toggleDropdown}
+            />
+            {showDropdown && (
+              <div className="dropdown-menu">
+                <button onClick={logout}>Logout</button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <FaUserCircle
+            size={30}
+            onClick={googleLogin}
+            style={{ cursor: "pointer" }}
+          />
+        )}
       </div>
     </div>
   );

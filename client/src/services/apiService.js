28ba51d822bcaa91616bf86d3cb82d2a9b70e1e8
Isuri Disanayaka <73772718+IsuriDisanayaka@users.noreply.github.com>
2024-05-13 23:07:59 +0530

    Deployment (#13)
    * Add Vercel configuration for COOP headers
    
    * depolyment
    
    * depolyment

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = process.env.BASE_URL;
 
 export const fetchEvents = async (email) => {
   const response = await axios.get(
